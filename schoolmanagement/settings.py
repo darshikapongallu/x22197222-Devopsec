ac8cad32287f109af7ea304c8601771f461bf5d7
darshikapongallu456 <darshikapongallu456@gmail.com>
2023-12-08 01:12:32 +0530

    Initial Commit

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'k0ujs9pcw+7qohwas!o7_ept20$c@$)-b=qco8sgviy_f)((bc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://x22197222-sms-env-env.eba-pbcx8qvm.eu-west-1.elasticbeanstalk.com/]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
