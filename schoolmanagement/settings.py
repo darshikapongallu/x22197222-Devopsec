3b797a19cf2877813af8f394cf99aec2ff825bff
darshikapongallu456 <darshikapongallu456@gmail.com>
2023-12-08 18:14:59 +0530

    Updated settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -121,15 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS=[
-# STATIC_DIR,
-#  ]
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_DIRS=[
+STATIC_DIR,
+ ]
+
+
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
